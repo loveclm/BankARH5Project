fc984e277592583ebdc4899cb7724f4294444a9d
loveclm <loveclm@126.com>
2017-06-26 07:54:15 -0700

    0626 work(1)
    - modify iPhone camera capture function
    - modify scan processing in android
    - modify some text.

--- js/mylocation.js
+++ js/mylocation.js
@@ -16,18 +16,53 @@ var timer = null;
 
 //var tagetPoint =new BMap.Point( 124.15,39.866667);
 //var centerPoint =new BMap.Point( 124.15,39.866667);
-var tagetPoint =new BMap.Point(120.2162672176,30.2439083354);
-var centerPoint =new BMap.Point(120.2162672176,30.2439083354);
+var tagetPoint =new BMap.Point( 120.2162672176, 30.2439083354);
+var centerPoint =new BMap.Point( 120.2162672176, 30.2439083354);
 var interval = 3000;
 var zoom = 15;
-
+var previousPoint = null;
 var index = 0;  //for test
 $(document).ready(function(){
 
     map = new BMap.Map("container");
     geolocation = new BMap.Geolocation();
+    map.enableScrollWheelZoom();
+    map.enableContinuousZoom();
 
     map.centerAndZoom(centerPoint, zoom);
+/*
+    var overlays = [];
+    var overlaycomplete = function(e){
+        overlays.push(e.overlay);
+    };
+    var styleOptions = {
+        strokeColor:"red",    //边线颜色。
+        fillColor:"red",      //填充颜色。当参数为空时，圆形将没有填充效果。
+        strokeWeight: 3,       //边线的宽度，以像素为单位。
+        strokeOpacity: 0.8,    //边线透明度，取值范围0 - 1。
+        fillOpacity: 0.6,      //填充的透明度，取值范围0 - 1。
+        strokeStyle: 'solid' //边线的样式，solid或dashed。
+    }
+
+    //实例化鼠标绘制工具
+    var drawingManager = new BMapLib.DrawingManager(map, {
+        isOpen: false, //是否开启绘制模式
+        //enableDrawingTool: true, //是否显示工具栏
+        drawingToolOptions: {
+            anchor: BMAP_ANCHOR_TOP_RIGHT, //位置
+            offset: new BMap.Size(5, 5), //偏离值
+        },
+        circleOptions: styleOptions, //圆的样式
+        polylineOptions: styleOptions, //线的样式
+        polygonOptions: styleOptions, //多边形的样式
+        rectangleOptions: styleOptions //矩形的样式
+    });
+
+    //添加鼠标绘制工具监听事件，用于获取绘制结果
+    drawingManager.addEventListener('overlaycomplete', overlaycomplete);
+
+*/
+
     if (!timer) {
         take_location();
         timer = setInterval( take_location, interval );
@@ -38,19 +73,44 @@ function take_location() {
     geolocation.getCurrentPosition(function(r){
         if(this.getStatus() == BMAP_STATUS_SUCCESS){
             var curPoint = r.point;
-
+            var distance = 0;
             ///// For test ///
             //curPoint.lat += index * 0.1;
             //console.log(curPoint);
             //index++;
+
+            //  curPoint.lat = 30.2844280000;
+             //  curPoint.lng = 120.0371370000;
+
+               //curPoint.lat = 30.2440880000;
+              // curPoint.lng = 120.2168520000;
+
+            if(previousPoint != null) {
+
+                distance = map.getDistance(curPoint,previousPoint);
+            }
+
+            //console.log(distance);
+            //console.log(map.getDistance(curPoint,tagetPoint));
             ////////
 
-            if(walking) map.clearOverlays();
-            walking = new BMap.WalkingRoute(map, {renderOptions:{map: map, autoViewport: true}});
-            walking.search(curPoint, tagetPoint);
+            if(previousPoint == null || distance > 100){
 
-        } else {
-            alert('??????~');
+                if(walking) map.clearOverlays();
+                walking = new BMap.WalkingRoute(map, {renderOptions:{map: map, autoViewport: true}});
+                walking.search(curPoint, tagetPoint);
+
+                walking.setSearchCompleteCallback(function(){
+                    var pts = walking.getResults().getPlan(0).getRoute(0).getPath();
+
+                    var polyline = new BMap.Polyline(pts, {strokeColor:"green", strokeWeight:4, strokeOpacity:0.5, strokeStyle:"solid"});
+                    map.addOverlay(polyline);
+
+                });
+
+
+                previousPoint = curPoint;
+            }
         }
     },{enableHighAccuracy: true});
 }
--- js/scan.js
+++ js/scan.js
@@ -3,12 +3,22 @@
  */
 //var apiKey = 'd9318ea05c'
 //var apiClientId = '46895';
+
+var canvas;
+var video;
+
 var apiMode = 'single';
 var apiScale = 480;
 var apiIsAll = 'best';
 var is_processing = false;
 
+var recog_count = 0;
+var check_camera = true;
+
 window.addEventListener('load',function(){
+    canvas = document.querySelector('#canvas');
+    video = document.querySelector('#video');
+
     var width = window.innerWidth
         || document.documentElement.clientWidth
         || document.body.clientWidth;
@@ -17,10 +27,13 @@ window.addEventListener('load',function(){
         || document.documentElement.clientHeight
         || document.body.clientHeight;
 
+    camera_enumerate();
 
     // scan dialog
     $('.scan').click(function(){
         show_scan_modal();
+        turnon_camera();
+        $('.videoWrapper').show();
         $('#scan_capture_img').hide();
     })
 
@@ -29,6 +42,8 @@ window.addEventListener('load',function(){
         $('#scan_capture_img').hide();
         $('.scan_btn_wrap').hide();
         $('#lightbox').hide();
+        clearInterval(recog_timmer);
+        recog_count = 0;
     });
 
     $("#frame").load(function(){
@@ -69,7 +84,8 @@ window.addEventListener('load',function(){
                         console.log(result);
 
                         if( result == 'h5ar_catch' ){
-                            show_success_modal();
+                            //show_success_modal();
+                            find_recog_show();
                             return;
                         }
                     }else{
@@ -96,23 +112,42 @@ window.addEventListener('load',function(){
 
     // scan button
     $('#scan_btn').click(function(){
-        var camera = document.getElementById('camera');
-        var frame = document.getElementById('frame');
+        if(videoStatus == false || check_camera == false){
+            var camera = document.getElementById('camera');
+            var frame = document.getElementById('frame');
 
-        $('#scan_before_img').hide();
-        $('.scan_btn_wrap').hide();
-        $('.recog_process').show();
+            $('#scan_before_img').hide();
+            $('.scan_btn_wrap').hide();
+            $('.recog_process').show();
 
-        camera.addEventListener('change', function(e) {
+            camera.addEventListener('change', function(e) {
 
-            var file = e.target.files[0];
-            frame.src = URL.createObjectURL(file);
+                var file = e.target.files[0];
+                frame.src = URL.createObjectURL(file);
 
-            $('#scan_capture_img').attr('src', URL.createObjectURL(file));
-            $('#scan_capture_img').show();
-        });
+                $('#scan_capture_img').attr('src', URL.createObjectURL(file));
+                $('#scan_capture_img').show();
+            });
 
-        $('#camera').click();
+            $('#camera').click();
+        } else {
+            recog_timmer = setInterval( function(){
+                recog_count++;
+                if( recog_count > 2 ){
+                    clearInterval( recog_timmer );
+                    cant_recog_show();
+                } else if( recog_count < 2 ){
+                    capture_recognitioon();
+                }
+
+            }, 4000 );
+            show_camera();
+
+            $('#scan_result').click(function(){
+                show_success_modal();
+            })
+        }
+
 
     })
 
@@ -135,6 +170,7 @@ scan dialog functions
 function show_scan_modal(){
     $('#lightbox').show();
     $('#scan_before_img').show();
+    $('.camera-wrap').hide();
     $('.scan_btn_wrap').show();
     $('.lightbox-content').css('margin-top', '15%');
     $('.lightbox-content').css('height', '67%');
@@ -144,20 +180,267 @@ function show_success_modal(){
     window.location = 'page6.html';
 }
 
+function show_camera(){
+    $('#scan_before_img').hide();
+    $('.camera-wrap').show();
+    $('.scan_btn_wrap').hide();
+}
+
+var is_find = false;
+function find_recog_show(){
+    $('.videoWrapper').hide();
+    $('#scan_capture_img').hide();
+    $('.recog_process').hide();
+    $('.cant_recog_wrap').show();
+
+    is_find = true;
+    $('#final_recog_show').show();
+    $('#cant_recog_show').hide();
+    recog_count = 0;
+    is_processing = false;
+    clearInterval(recog_timmer);
+}
+
 function cant_recog_show(){
     $('.videoWrapper').hide();
     $('#scan_capture_img').hide();
     $('.recog_process').hide();
     $('.cant_recog_wrap').show();
+
+    is_find = false;
+    $('#final_recog_show').hide();
+    $('#cant_recog_show').show();
     recog_count = 0;
     is_processing = false;
     clearInterval(recog_timmer);
 }
+
 function cant_recog_hide(){
+    if( is_find == true ){
+        window.location = 'page6.html';
+        return;
+    }
     $('.cant_recog_wrap').hide();
     $('#lightbox').hide();
-
 }
 
-var recog_timmer;
 
+
+
+
+function camera_enumerate(){
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.mediaDevices.getUserMedia;
+
+    var rearCameraId;
+
+    if (navigator.mediaDevices === undefined ||
+        navigator.mediaDevices.enumerateDevices === undefined) {
+        //alert('这个浏览器不支持MediaStreamTrack。\n\n请使用360阅览器.');
+        check_camera = false;
+    } else {
+        navigator.mediaDevices.enumerateDevices()
+            .then(function(devices) {
+                devices.forEach(function(device) {
+                    if (device.kind == 'videoinput') {
+                        camera_ids.push(device.deviceId);
+                    }
+                });
+            })
+            .catch(function(err) {
+                //alert(err.name + ": " + err.message);
+                check_camera = false;
+            });
+    }
+}
+
+
+var videoStatus = false;
+var videoElement;
+var camera_ids = new Array();
+function turnon_camera(){
+    video.classList.remove('hidden');
+
+    videoElement = document.querySelector('video');
+
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.mediaDevices.getUserMedia;// Older browsers might not implement mediaDevices at all, so we set an empty object first
+    if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+    }
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function(constraints) {
+
+            // First get ahold of the legacy getUserMedia, if present
+            var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+            // Some browsers just don't implement it - return a rejected promise with an error
+            // to keep a consistent interface
+            if (!getUserMedia) {
+                return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+            }
+
+            // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+            return new Promise(function(resolve, reject) {
+                getUserMedia.call(navigator, constraints, resolve, reject);
+            });
+        }
+    }
+
+
+    var rearCameraId;
+
+    if (navigator.mediaDevices === undefined ||
+        navigator.mediaDevices.enumerateDevices === undefined) {
+        //alert('这个浏览器不支持MediaStreamTrack。\n\n请使用360阅览器.');
+        check_camera = false;
+    } else {
+        try{
+            if( camera_ids.length > 0 ){
+                rearCameraId = camera_ids[camera_ids.length-1];
+                var constraints = {
+                    video: {
+                        optional: [{sourceId: rearCameraId}],
+                        mandatory: {
+                            maxWidth: 480,
+                            maxHeight: 480,
+                            minWidth: 480,
+                            minHeight: 480
+                        }
+                    },
+                    audio: false
+                };
+                navigator.getUserMedia(constraints, successCallback, errorCallback);
+            }
+        }
+        catch(err){
+            //alert(JSON.stringify(err));
+            check_camera = false;
+        }
+    }
+}
+
+
+
+var recog_timmer;
+function successCallback(stream) {
+    videoStatus = true;
+    window.stream = stream; // make stream available to console
+    videoElement.src = window.URL.createObjectURL(stream);
+    videoElement.play();
+}
+
+function errorCallback(error) {
+    videoStatus = false;
+    //alert('navigator.getUserMedia error: ' + error);
+    check_camera = false;
+}
+
+
+
+function capture_recognitioon(){
+    //video.addEventListener('click', function(e){
+    if (videoStatus) {
+        $('#capture').show();
+        var imageBase64 = capture(apiScale);
+    } else {
+        alert('Image capturing error.');
+        return;
+    }
+
+    if (imageBase64 == null) {
+        alert("Image data processing error.");
+        return;
+    } else {
+        //Recognize.im API options.
+        var apiOpt = {
+            clientId: apiClientId,
+            key: apiKey,
+            mode: apiMode,
+            allResults: apiIsAll,
+            debug: true,
+            onRecognitionSuccess: function(resp) {
+                if(recog_count == 0){
+                    return;
+                }
+                if( resp.status == 0 ){
+                    var result = resp.objects[0];
+                    result = result.id;
+
+                    if( result == 'h5ar_catch' ){
+                        //show_success_modal();
+                        find_recog_show();
+                        return;
+                    }
+                }
+
+                cant_recog_show();
+            },
+            onRecognitionFailure: function(resp) {
+                if(recog_count == 0){
+                    return;
+                }
+
+                cant_recog_show();
+                $('#canvas').hide();
+                is_processing = false;
+            },
+            onError: function(ajax) {
+                if(recog_count == 0){
+                    return;
+                }
+
+                cant_recog_show();
+            }
+        }
+        //Init Recognize.im API object and send request.
+        var recognizeIm = new RecognizeIm(apiOpt);
+        if( is_processing == false )
+            recognizeIm.recognize(imageBase64);
+        is_processing = true;
+    }
+};
+
+function capture(scale) {
+    var width = video.videoWidth;
+    var height = video.videoHeight;
+    if (scale && height > scale) {
+        value = scale / width;
+        width = scale;
+        height = height * value;
+    }
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, width, height);
+    var image = canvas.toDataURL('image/jpeg');
+    image = image.replace('data:image/jpeg;base64,', '');
+    return image;
+}
+
+if (navigator.mediaDevices === undefined) {
+    navigator.mediaDevices = {};
+}
+
+if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+
+        // First get ahold of the legacy getUserMedia, if present
+        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        // Some browsers just don't implement it - return a rejected promise with an error
+        // to keep a consistent interface
+        if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+
+        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+        return new Promise(function(resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+    }
+}
